3976a08b2952cdd22425271c49491a61320d9fc4
Zane Ives <deni23n@gmail.com>
2022-10-25 16:49:49 +0200

    sync with main

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  selectedHero?: Hero
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-
-  heroes = HEROES
-
-  constructor() { 
-    
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
+// export class HeroesComponent implements OnInit {
+
+//   constructor(private heroService:HeroService, public messageService:MessageService) { 
+//   }
+
+//   ngOnInit(): void {
+//     this.getHeroes()
+//   }
+
+//   selectedHero?: Hero
+//   heroes: Hero[] = []
+
+//   // onSelect(hero: Hero): void {
+//   //   this.selectedHero = hero;
+//   // }  
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+//   }
+
+//   // getHeroes(): void {
+//   //   this.heroes = this.heroService.getHeroes();
+//   // }  
+
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }
+  
+// }
