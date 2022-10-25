87b20ff85b33aa065c14a47233c868a3c0bcf9f6
zaneives <deni23n@gmail.com>
2022-10-25 17:48:02 +0200

    sync to repo

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,15 +10,22 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService:MessageService) { }
-  
+  constructor(private messageService: MessageService) { }
+
   // getHeroes(): Hero[] {
-    //   return HEROES;
-    // }
-    
-    getHeroes(): Observable<Hero[]> {
-      const heroes = of(HEROES);
-      this.messageService.add('HeroService: fetched heroes');
-      return heroes;
-    }
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
 }
